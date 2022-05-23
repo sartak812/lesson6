c0306b40037e7162887b376bc4f8121500731cbb
artsargsyan <sartak812@gmail.com>
2022-05-23 21:44:24 +0400

    Chenage omthing in basht

--- myfirstbash.sh
+++ myfirstbash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 AGE=16
-echo" What is your name"
-read -p name
+#echo "What is your name:"
+read -p "What is your name:" name
 echo "How old are you?"
 read -p AGE
 echo "Your name is $name and you are $AGE years old."
